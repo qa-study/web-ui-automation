cf9a17360688487cbc268a37aa481328bb6f99b0
Jimin <jjimini98@naver.com>
2025-01-16 20:50:40 +0900

    refactor : element 에 있는 요소로 변경

--- pages/탭_내비게이션.py
+++ pages/탭_내비게이션.py
@@ -29,4 +29,4 @@ class 탭_내비게이션(BasePage):
 
     def creators_page_redirect(self):
         BasePage.click_element(self, By.XPATH, element.xpath_creators_button)
-        BasePage.driver_title(self, "네이버 : 로그인")
+        BasePage.driver_title(self, By.XPATH, element.xpath_webtoon_naverlogin_title)
